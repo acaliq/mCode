f1b90447efe87ac734617e4923257e214ca7fca9
Acaliq <acaliq6@gmail.com>
2022-08-04 09:26:28 +0800

    del files

--- mPrint.py
+++
@@ -1,7 +0,0 @@
-def show(stri = 'None typed'):
-    print(stri)
-show()
-
-print("Nonono")
-
-x = 1
--- mPrint2.py
+++
@@ -1,6 +0,0 @@
-# Deal with the format of Greek Letters
-import pandas as pd
-rfile = open('greek_letter.txt')
-mfile = pd.read_csv('greek_letter.txt')
-mfile.to_csv('greek_letter2.csv')
-print('Suc')
